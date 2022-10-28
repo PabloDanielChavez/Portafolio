5b41a141d64de41a5412d5656f10df22bb4455cf
Pablo <pablo_daniel_chavez@outlook.es>
2022-10-28 19:09:32 -0300

    Carpeta Nueva

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.swiper = new Swiper({
+    el: '.loop__contenedor',
+    slideClass: 'loop__slide',
+    createElements: true,
+    autoplay: {
+      delay: 5000
+    },
+    loop: true,
+    pagination: true,
+    navigation: true,
+});
