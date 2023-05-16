38fe3b66df78bbb03ae2f68fcc0cfafa88d5b992
Pablo <Pablo_daniel_chavez@outlook.es>
2023-05-16 10:47:15 -0300

    Alerta de cambio

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
 
 import {
-            cargarIndex,
-            cargarInformacion,
-        } from './funciones.js';
+    cargarIndex,
+    cargarInformacion,
+} from './funciones.js';
 
-import { 
-            pantallaDeCarga
-        } from './variables.js';
+import {
+    pantallaDeCarga
+} from './variables.js';
 
 (function () {
-    if(window.location.href === 'http://127.0.0.1:5500/index.html' || window.location.href === 'https://portafolio-pdc.netlify.app' || window.location.href === 'https://portafolio-pdc.netlify.app/' || window.location.href === 'https://portafolio-pdc.netlify.app/index.html') {
+    alert("El portafolio está obsoleto, Se está trabajando en una nueva versión.");
+    if (window.location.href === 'http://127.0.0.1:5500/index.html' || window.location.href === 'https://portafolio-pdc.netlify.app' || window.location.href === 'https://portafolio-pdc.netlify.app/' || window.location.href === 'https://portafolio-pdc.netlify.app/index.html') {
         document.addEventListener("DOMContentLoaded", cargarIndex);
         setTimeout(() => {
             pantallaDeCarga.remove();
         }, 1000);
-    } else if(window.location.href === 'http://127.0.0.1:5500/informacion.html' || window.location.href === 'https://portafolio-pdc.netlify.app/informacion.html' || window.location.href === 'https://portafolio-pdc.netlify.app/informacion') {
+    } else if (window.location.href === 'http://127.0.0.1:5500/informacion.html' || window.location.href === 'https://portafolio-pdc.netlify.app/informacion.html' || window.location.href === 'https://portafolio-pdc.netlify.app/informacion') {
         document.addEventListener("DOMContentLoaded", cargarInformacion);
         setTimeout(() => {
             pantallaDeCarga.remove();
